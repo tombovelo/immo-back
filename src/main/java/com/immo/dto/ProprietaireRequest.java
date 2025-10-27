d9dad8051dd5be6955b2105cdf7aa4ea0095f6ac
tombovelo <agnontsoa@gmail.com>
2025-10-27 13:32:37 +0300

    mis a jour de la validation

--- src/main/java/com/immo/dto/ProprietaireRequest.java
+++ src/main/java/com/immo/dto/ProprietaireRequest.java
@@ -9,14 +9,14 @@ import lombok.Data;
 @Data
 public class ProprietaireRequest {
     // ✅ Nom obligatoire avec validation
-    @NotBlank(message = "Le nom est obligatoire")
+    @NotBlank(message = "nom obligatoire")
     private String nom;
     private String prenom;
     @Pattern(regexp = "^(\\+261|0)[0-9]{9}$", message = "Le numero doit etre de la forme +261342123236 ou 0326124546")
-    @NotBlank(message = "Le numero de telephone obligatoire")
+    @NotBlank(message = "telephone obligatoire")
     private String telephone;
     private String adresse;
-    @NotNull(message = "utilisateur est obligatoire")
+    @NotNull(message = "utilisateur obligatoire")
     @Valid
     private UtilisateurRequest utilisateur;
 }
